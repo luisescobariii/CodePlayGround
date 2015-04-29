cbb2517b21ec36e99f2464cf5e262a5edbce3985
luisescobariii <luis@rounintech.com>
2015-04-29 18:23:32 -0500

    Created by Coding Ground Client

---
+++ TallerLogica3/TallerLogica3.java
@@ -0,0 +1,34 @@
+class TallerLogica3{
+    
+    public static void main (String[] args) throws java.lang.Exception {new TallerLogica3();}
+	
+	public TallerLogica3(){
+	    
+		Console.println("Seleccione el ejercicio que desea ejecutar y oprima 'Enter'.\n"+
+						"1. Crear e imprimir un vector.\n"+
+						"2. Crear un vector aleatorio y hallar y mostrar el mayor valor,\n"+
+						"   sus posiciones, cuantas veces se repite y los multiplos de 3.\n"+
+						"3. Crear un vector y sumar los valores pares e impares por separado.\n"+
+						"4. Crear y dividir un vector en dos vectores que contengan los\n"+
+						"   valores mayores y menores a su promedio.\n"+
+						"5. Crear un vector con los productos de los valores de otros dos\n"+
+						"   vectores en order paralelo.\n"+
+						"6. Crear un vector con los productos de los valores de otros dos\n"+
+						"   vectores en order opuesto.\n"+
+						"7. Crear un arreglo y crear metodos para: Mostrar el vector,\n"+
+						"   eliminar un elemento, buscar un elemento y retornarlo, buscar\n"+
+						"   un elemento y retornar si existe o no, modificar un elemento,\n"+
+						"   insertar un nuevo elemento, ordernar el vector.\n"+
+						"8. Crear y mostrar una matriz.\n"+
+						"9. Crear una matriz y metodos para: Mostrar la matriz, eliminar\n"+
+						"   una fila, eliminar una columna, calcular promedios, hallar\n"+
+						"   mayores y menores, insertar filas, insertar columnas, buscar\n"+
+						"   un dato, actualizar un dato.\n"+
+						"10.Crear un vector de objetos y crear metodos para: agregar y\n"+
+						"   eliminar objetos, mostrar los valores del vector.\n"+
+						"11.Crear un vector de objetos que aplique descuentos a valores\n"+
+						"   ingresados y muestre el total de los valores y el promedio\n"+
+						"   de los descuentos.");
+		Console.println(Console.readString());
+	}
+}
