8d3a18103f078ce2943fa30928485bde7ddb592a
luisescobariii <luisescobariii@gmail.com>
2020-02-18 19:10:57 -0500

    travelling salesman

---
+++ travelling-salesman/main.js
@@ -0,0 +1,16 @@
+let cities = [];
+
+function setup() {
+  createCanvas(700, 700);
+  background(0);
+  
+  for (let i = 0; i < 10; i++) {
+    cities.push(new pVector(Math.random() * width, Math.random() * heigth));
+  }
+}
+
+function draw() {
+  for (let city of cities) {
+    ellipse(city.x, city.y, 5);
+  }
+}
