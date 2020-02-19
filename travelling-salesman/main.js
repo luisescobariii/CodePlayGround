0f64bd93c5451a156f91a70ac1568973f5a48cac
luisescobariii <luisescobariii@gmail.com>
2020-02-18 19:21:24 -0500

    test1

--- travelling-salesman/main.js
+++ travelling-salesman/main.js
@@ -3,7 +3,7 @@ let cities = [];
 function setup() {
   createCanvas(700, 700);
   background(0);
-  
+
   for (let i = 0; i < 10; i++) {
     cities.push(new pVector(Math.random() * width, Math.random() * heigth));
   }
